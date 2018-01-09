2d1c6353220305810ff743f1340b228649a2a8a3
Spencer Wildes <spencerwildes@gmail.com>
2018-01-08 18:28:22 -0800

    2nd commit

--- Bamazon/BamazonCustomer.js
+++ Bamazon/BamazonCustomer.js
@@ -10,26 +10,26 @@ var padText = require('./padTable.js')
 
 // Link to mySQL Database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root", //Your username
-    password: "kingsley1", //Your password
-    database: "Bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root", //Your username
+  password: "kingsley1", //Your password
+  database: "Bamazon"
 });
 
 // Connect to Database
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
 });
 
 
 
 // Display All Items inside Database and sell an item to customer
-connection.query('SELECT * FROM Products', function(err, res){
-  
+connection.query('SELECT * FROM Products', function (err, res) {
+
   // Error Handler
-  if(err) throw err;
+  if (err) throw err;
 
 
   // Show User message
@@ -38,9 +38,9 @@ connection.query('SELECT * FROM Products', function(err, res){
   // Set up table header
   console.log('  ID  |      Product Name      |  Department Name  |   Price  | In Stock');
   console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
-  
+
   // Loop through database and show all items
-  for(var i = 0; i < res.length; i++){
+  for (var i = 0; i < res.length; i++) {
 
     // ---------- Add in padding for table ----------
     var itemID = res[i].ItemID + ''; // convert to string
@@ -70,7 +70,7 @@ connection.query('SELECT * FROM Products', function(err, res){
   // Ask for Item ID
   console.log('\nWhich item do you want to buy?');
   prompt.get(['buyItemID'], function (err, result) {
-    
+
     // Show Item ID selected
     var buyItemID = result.buyItemID;
     console.log('You selected Item # ' + buyItemID + '.');
@@ -84,55 +84,55 @@ connection.query('SELECT * FROM Products', function(err, res){
       console.log('You selected to buy ' + buyItemQuantity + ' of these.');
 
       // Once the customer has placed the order, check if store has enough of the product to meet the request
-      connection.query('SELECT StockQuantity FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){
-        if(err) throw err; // Error Handler
+      connection.query('SELECT StockQuantity FROM Products WHERE ?', [{ ItemID: buyItemID }], function (err, res) {
+        if (err) throw err; // Error Handler
         // Check if the item Id was valid (i.e. something was returned from mySQL)
-        if(res[0] == undefined){
-          console.log('Sorry... We found no items with Item ID "' +  buyItemID + '"');
+        if (res[0] == undefined) {
+          console.log('Sorry... We found no items with Item ID "' + buyItemID + '"');
           connection.end(); // end the script/connection
         }
         // Valid Item ID, so compare Bamazon Inventory with user quantity 
-        else{
+        else {
           var bamazonQuantity = res[0].StockQuantity;
           // Sufficient inventory
-          if(bamazonQuantity >= buyItemQuantity){
+          if (bamazonQuantity >= buyItemQuantity) {
 
             // Update mySQL database with reduced inventory
             var newInventory = parseInt(bamazonQuantity) - parseInt(buyItemQuantity); // ensure we have integers for subtraction & database
-            connection.query('UPDATE Products SET ? WHERE ?', [{StockQuantity: newInventory}, {ItemID: buyItemID}], function(err, res){
-              if(err) throw err; // Error Handler
+            connection.query('UPDATE Products SET ? WHERE ?', [{ StockQuantity: newInventory }, { ItemID: buyItemID }], function (err, res) {
+              if (err) throw err; // Error Handler
             }); // end inventory update query
 
 
             // Show customer their purchase total (need to query the price info from database)
             var customerTotal;
-            connection.query('SELECT Price FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){
-              
+            connection.query('SELECT Price FROM Products WHERE ?', [{ ItemID: buyItemID }], function (err, res) {
+
               var buyItemPrice = res[0].Price;
-              customerTotal = buyItemQuantity*buyItemPrice.toFixed(2);
+              customerTotal = buyItemQuantity * buyItemPrice.toFixed(2);
 
               console.log('\nYour total is $' + customerTotal + '.');
 
               // ------------------------- Re factor for Executive Challenge ------------------------
               // Find the department for the purchase item
-              connection.query('SELECT DepartmentName FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){
+              connection.query('SELECT DepartmentName FROM Products WHERE ?', [{ ItemID: buyItemID }], function (err, res) {
                 var itemDepartment = res[0].DepartmentName;
-                
+
                 // Find the current Revenue for that department
-                connection.query('SELECT TotalSales FROM Departments WHERE ?', [{DepartmentName: itemDepartment}], function(err, res){
+                connection.query('SELECT TotalSales FROM Departments WHERE ?', [{ DepartmentName: itemDepartment }], function (err, res) {
                   var totalSales = res[0].TotalSales;
 
                   // Calculate new sale revenue
                   var totalSales = parseFloat(totalSales) + parseFloat(customerTotal);
 
                   // Add the revenue from each transaction to the TotalSales column for the related department.
-                  connection.query('UPDATE Departments SET ? WHERE ?', [{TotalSales: totalSales}, {DepartmentName: itemDepartment}], function(err, res){
-                    if(err) throw err; // Error Handler
+                  connection.query('UPDATE Departments SET ? WHERE ?', [{ TotalSales: totalSales }, { DepartmentName: itemDepartment }], function (err, res) {
+                    if (err) throw err; // Error Handler
                     console.log('Transaction Completed. Thank you!')
                     connection.end(); // end the script/connection
 
                   }); // end new revenue update query
-      
+
                 }); // end current revenue query
 
               }); // end department name query 
@@ -140,8 +140,8 @@ connection.query('SELECT * FROM Products', function(err, res){
             }); // end customer purchase update query 
           }
           // Insufficient inventory
-          else{
-            console.log('Sorry... We only have ' +  bamazonQuantity + ' of that item. Order cancelled.');
+          else {
+            console.log('Sorry... We only have ' + bamazonQuantity + ' of that item. Order cancelled.');
             connection.end(); // end the script/connection
           }
         }
